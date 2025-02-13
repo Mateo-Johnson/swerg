4ce354f08f9c58fe2106cbf7b3477cd33c9a4c9b
Mateo Johnson <johnson.12@themetroschool.org>
2025-02-13 10:47:30 -0500

    updated configs for the same thing

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -11,10 +11,12 @@ public final class Configs {
         public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
 
         static {
-            // Driving configuration
+            // Use module constants to calculate conversion factors and feed forward gain.
             double drivingFactor = ModuleConstants.kWheelDiameterMeters * Math.PI
                     / ModuleConstants.kDrivingMotorReduction;
+            double turningFactor = 2 * Math.PI;
             double drivingVelocityFeedForward = 1 / ModuleConstants.kDriveWheelFreeSpeedRps;
+
             drivingConfig
                     .idleMode(IdleMode.kBrake)
                     .smartCurrentLimit(50);
@@ -27,10 +29,20 @@ public final class Configs {
                     .pid(0.04, 0, 0)
                     .velocityFF(drivingVelocityFeedForward)
                     .outputRange(-1, 1);
-            // Turning configuration
-            turningConfig
+                    turningConfig
                     .idleMode(IdleMode.kBrake)
                     .smartCurrentLimit(20);
+                
+                turningConfig.encoder  // Use the built-in encoder
+                    .positionConversionFactor(turningFactor)    // radians
+                    .velocityConversionFactor(turningFactor / 60.0);  // radians per second
+                
+                turningConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)  // Use the built-in encoder
+                    .pid(1, 0, 0)
+                    .outputRange(-1, 1)
+                    .positionWrappingEnabled(true)
+                    .positionWrappingInputRange(0, turningFactor);
         }
     }
 }
