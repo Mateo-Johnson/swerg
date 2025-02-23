0ce849d48a21d278a16fca1cfb339fafe852a3fc
Mateo Johnson <johnson.12@themetroschool.org>
2025-02-23 12:30:08 -0500

    finished alignreefleft

--- src/main/java/frc/robot/subsystems/vision/commands/game/AlignReefLeft.java
+++ src/main/java/frc/robot/subsystems/vision/commands/game/AlignReefLeft.java
@@ -1,22 +1,15 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.vision.commands.game;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.commands.general.AlignX;
-import frc.robot.utils.Constants.PIDConstants;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AlignReefLeft extends Command {
   /** Creates a new AlignReefLeft. */
   private final Drivetrain m_drivetrain;
   private final Vision m_vision;
-  private final PIDController reefPID = PIDConstants.translateController;
 
   public AlignReefLeft(Drivetrain drivetrain, Vision vision) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -33,7 +26,7 @@ public class AlignReefLeft extends Command {
   @Override
   public void execute() {
     double tX = m_vision.getTX();
-    double setpoint = 0;
+    double setpoint = -5;
     
     new AlignX(setpoint, tX, 0.1, m_drivetrain).schedule();
   }
