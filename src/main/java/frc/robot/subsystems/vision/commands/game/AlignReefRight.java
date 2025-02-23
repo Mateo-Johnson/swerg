936d1b20efd5fb6528ccf403f12e5d3e6eeeb7ea
Mateo Johnson <johnson.12@themetroschool.org>
2025-02-23 12:32:02 -0500

    NO ONE MAN SHOULD HAVE ALL THAT POWERRR, THE CLOCKS TICKIN I JUST COUNT THE HOURSSSS, STOP TRIPPIN IM TRIPPIN OFF THE POWERSSSSS, TILL THEN **** THAT THE WORLDS OURSSSSSSSSS

--- src/main/java/frc/robot/subsystems/vision/commands/game/AlignReefRight.java
+++ src/main/java/frc/robot/subsystems/vision/commands/game/AlignReefRight.java
@@ -1,18 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.vision.commands.game;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.commands.general.AlignX;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AlignReefRight extends Command {
   /** Creates a new AlignReefLeft. */
   private final Drivetrain m_drivetrain;
   private final Vision m_vision;
+
   public AlignReefRight(Drivetrain drivetrain, Vision vision) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.m_drivetrain = drivetrain;
@@ -26,7 +24,12 @@ public class AlignReefRight extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double tX = m_vision.getTX();
+    double setpoint = 5;
+    
+    new AlignX(setpoint, tX, 0.1, m_drivetrain).schedule();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
