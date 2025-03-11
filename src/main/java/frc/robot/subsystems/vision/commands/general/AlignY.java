55a8baa1bb81aea50882ca0f674c5e9f3b0ef841
Mateo Johnson <johnson.12@themetroschool.org>
2025-03-11 09:37:42 -0400

    update

--- src/main/java/frc/robot/subsystems/vision/commands/general/AlignY.java
+++ src/main/java/frc/robot/subsystems/vision/commands/general/AlignY.java
@@ -2,24 +2,16 @@ package frc.robot.subsystems.vision.commands.general;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.utils.Constants;
-import frc.robot.utils.Constants.OIConstants;
 import frc.robot.utils.LimelightLib;
 
 public class AlignY extends Command {
   private final Drivetrain drivetrain;
+  private final double setpoint;
   private final PIDController yPID = new PIDController(0.015, 0.0001, 0);
-  private final String limelightName = "limelight-front";
   
-  // Target ID to track
-  private final int targetID;
   
   /**
    * Creates a new AlignY command that aligns to a specific AprilTag/target.
@@ -27,8 +19,8 @@ public class AlignY extends Command {
    * @param targetID The ID of the target to align with
    * @param drivetrain The drivetrain subsystem to control
    */
-  public AlignY(int targetID, Drivetrain drivetrain) {
-    this.targetID = targetID;
+  public AlignY(double setpoint, Drivetrain drivetrain) {
+    this.setpoint = setpoint;
     this.drivetrain = drivetrain;
     
     // Configure PID controller
@@ -59,14 +51,14 @@ public class AlignY extends Command {
     double lateralOffset = targetPose[1];
     
     // Our goal is to make this lateral offset zero
-    double output = yPID.calculate(lateralOffset, 0);
+    double output = yPID.calculate(lateralOffset, setpoint);
     
     // Clamp the output to valid range
     output = MathUtil.clamp(output, -0.5, 0.5);
     
     // Apply the steering correction while maintaining forward/backward control from driver
     drivetrain.drive(
-      MathUtil.applyDeadband(Constants.primary.getLeftY(), OIConstants.kDriveDeadband), 
+      0, 
       -output, // Negate because positive output should turn robot right to align left
       0, 
       false);
