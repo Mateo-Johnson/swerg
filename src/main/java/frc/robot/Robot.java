0292464d3de0ce5ef165ed22c1e3a27d1a8d213e
Mateo Johnson <johnson.12@themetroschool.org>
2025-03-26 09:55:05 -0400

    added camera i think

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -16,6 +17,10 @@ import frc.robot.utils.Elastic;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+
+  public Robot() {
+    CameraServer.startAutomaticCapture();
+  }
 
   private RobotContainer m_robotContainer;
 
